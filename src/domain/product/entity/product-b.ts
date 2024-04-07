993e43cabd4ec9664ad25fb7791d47282e567f09
abeloti <alisson.beloti@encontact.com.br>
2024-04-07 15:45:12 -0300

    2 erros + notification

--- src/domain/product/entity/product-b.ts
+++ src/domain/product/entity/product-b.ts
@@ -1,19 +1,17 @@
+import Entity from "../../@shared/entity/entity.abstract";
 import ProductInterface from "./product.interface";
 
-export default class ProductB implements ProductInterface {
-  private _id: string;
+export default class ProductB extends Entity implements ProductInterface {
+  
   private _name: string;
   private _price: number;
 
   constructor(id: string, name: string, price: number) {
+    super();
     this._id = id;
     this._name = name;
     this._price = price;
     this.validate();
-  }
-
-  get id(): string {
-    return this._id;
   }
 
   get name(): string {
